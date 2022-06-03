3e822a2a04a3577ddc6c91204c044602fc89b1f9
Gabriel Pinheiro <gabrielpsilvas@live.com>
2022-06-03 15:08:04 -0300

    feat: added initial base widget component

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,6 @@
-import { ReactNode } from "react"
-
-type ButtonProps = {
-  text?: string
-}
-
-function Button ({ text = 'Default' }: ButtonProps) {
-  return <button className="bg-violet-500 text-violet-100 p-2 h-10 rounded hover:bg-violet-700 transition-colors">
-    {text}
-  </button>
-}
+import { Widget } from "./components"
 
 export function App() {
-  return (
-    <>
-      <h1>Feedget</h1> 
-      <div className="flex gap-2">
-        <Button text='Enviar' />
-        <Button text='Ok' />
-        <Button  />
-      </div>
-    </>
-  )
-}
+  return <Widget />}
 
 export default App
